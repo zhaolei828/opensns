c8c11f7121edf5ddbea03b6fe4277d4b22d26f94
zhaolei <zhaolei@chinadrtv.com>
2015-08-19 16:27:55 +0800

    自定义模板

---
+++ Theme/Light/info.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+    //模版名
+    'name' => 'Light',
+    //别名
+    'title' => '小清新',
+    //版本号
+    'version' => '1.0.0',
+    //是否商业模版,1是，0，否
+    'is_com' => 0,
+    //模版描述
+    'summary' => '参考Wecenter的小清新风格。',
+    //开发者
+    'developer' => '嘉兴想天信息科技有限公司',
+    //开发者网站
+    'website' => 'http://www.ourstu.com',
+);
